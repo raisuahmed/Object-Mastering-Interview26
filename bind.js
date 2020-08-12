b38067ede59bcc097edfbfc09aca50ec26525e71
raisuahmed <raisuahmed83@gmail.com>
2020-08-12 08:22:43 -0700

    conslog.this

--- bind.js
+++ bind.js
@@ -3,15 +3,24 @@ const normalPerson = {
     lastName:'Uddin',
     salary: 15000,
     getFullName: function(){
+      
         console.log(this.firstName, this.lastName);
     },
    chargeBill: function(amount){
-       console.log(this);
+      
         this.salary = this.salary - amount; 
         return this.salary;
     }
 
-} 
-normalPerson.chargeBill(1500);
-normalPerson.chargeBill(1500);
- console.log(normalPerson.salary);
+    } 
+
+    const heroPerson = {
+        firstName:'rasel',
+        lastName:'Ahmed',
+        salary : 23000,
+    }
+
+    const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+    heroChargeBill(2000);
+    heroChargeBill(1000);
+    console.log(heroPerson.salary);
