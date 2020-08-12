ce4a929dbad01487ced66b76c8b881c456997580
raisuahmed <raisuahmed83@gmail.com>
2020-08-12 08:27:36 -0700

    normal person and hero

--- bind.js
+++ bind.js
@@ -7,7 +7,7 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
    chargeBill: function(amount){
-      
+      console.log(this);
         this.salary = this.salary - amount; 
         return this.salary;
     }
@@ -23,4 +23,8 @@ const normalPerson = {
     const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
     heroChargeBill(2000);
     heroChargeBill(1000);
+    normalPerson.chargeBill(1500);
+    normalPerson.chargeBill(1500);
     console.log(heroPerson.salary);
+    
+    console.log(normalPerson.salary);
