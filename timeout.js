59c71b2db47856b1e663d2d508cb254f9c6a98f1
raisuahmed <raisuahmed83@gmail.com>
2020-08-13 19:21:55 -0700

    work done

---
+++ timeout.js
@@ -0,0 +1,59 @@
+
+//syncronous
+// function doSomething() {
+//     console.log(3333);
+
+// }
+// console.log(2222);
+// doSomething();
+// console.log(4444);
+// console.log(4444);
+
+// //set time out
+
+// function doSome() {
+//     console.log(3333);
+    
+// }
+// console.log(2222);
+// setTimeout(doSome, 3000);
+// console.log(44444);
+// console.log(44444);
+// console.log(4444);
+
+// function doSomething() {
+//     console.log(3333);
+// }
+// console.log(2222);
+// setTimeout(function() {
+//     console.log('lazy and waiting')
+// },4000)
+    
+// console.log(44444);
+// console.log(44444);
+
+//arrow dara
+
+function doSomething() {
+    console.log(3333);
+}
+console.log(2222);
+setTimeout(() => {
+    
+ console.log('lazy and waiting for')
+    },4000)
+        
+    console.log(44444);
+    console.log(44444);
+    
+    //set Interval
+
+    function doSomething() {
+        console.log(3333);
+    }
+    console.log(2222);
+    setInterval(() => {
+        console.log('doing it');
+    }, 3000);
+
+    console.log(40000);
